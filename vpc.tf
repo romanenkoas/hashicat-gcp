728d779a0525ff67e3399f2011ea986e0fb47fbc
romanenkoas85 <romanenkoas85@gmail.com>
2021-05-27 10:43:35 +0000

    tmp fix 5

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+    source  = "network/google"
+    version = "3.2.2"
 
     project_id   = var.project
     network_name = "gaurav-network"
